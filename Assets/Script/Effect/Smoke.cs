757d2c8a238ec7723dd60cfec043f16e9150cda0
tooyama <toyana@dcl.cs.waseda.ac.jp>
2016-04-24 18:39:13 +0900

    UI追加

---
+++ Assets/Script/Effect/Smoke.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace WCE_16
+{
+    public class Smoke : MonoBehaviour
+    {
+        public float exhaustRate;
+
+        [SerializeField]
+        private float engineRevs;
+        [SerializeField]
+        private ParticleSystem exhaust;
+
+
+        void Start() 
+        {
+            exhaust = GetComponent<ParticleSystem>();
+        }
+
+
+        void Update()
+        {
+            exhaust.emissionRate = engineRevs * exhaustRate;
+        }
+        /*
+        void FixedUpdate()
+        {
+            engineRevs = Mathf.Abs(transform.parent.GetComponent<CharacterController>().getH());
+        }
+        */
+    }
+}
